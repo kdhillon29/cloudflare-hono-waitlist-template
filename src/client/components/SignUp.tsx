175c02f56eafcd1bc98b8b846bf1bea6331a9cf1
kan dhillon <kdhillon469@gmail.com>
2026-02-03 16:09:36 +1100

    add signup component

---
+++ src/client/components/SignUp.tsx
@@ -0,0 +1,25 @@
+import { Mail } from "lucide-react";
+import Countdown from "./Countdown";
+
+export const Signup = () => {
+  return (
+    <div className="max-w-md text-center">
+      <div className="join">
+        <div>
+          <label className="input validator join-item">
+            <Mail className="text-neutral-content" />
+            <input type="email" placeholder="mail@site.com" required />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+        </div>
+        <button className="btn btn-neutral join-item">Join</button>
+      </div>
+
+      <p className="text-accent dark:text-accent mt-3 w-full text-center text-sm font-semibold">
+        Join Our Waiting List to get early access and special discounts!
+      </p>
+    </div>
+  );
+};
+
+export default Signup;
