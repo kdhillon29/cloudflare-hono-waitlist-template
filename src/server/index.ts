5e711187030e17074ef12a856e1e63c113725ee7
kan dhillon <kdhillon469@gmail.com>
2026-02-04 20:51:23 +1100

    add astro endpoints

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,38 @@
 import { Hono } from "hono";
 import { accessAuth } from "./middleware/auth";
+import type { D1Database } from "@cloudflare/workers-types";
+import { insertSubscriber, isSubscriberExists } from "./db/queries";
+import { zValidator } from "@hono/zod-validator";
+import { insertSubscriberSchema } from "../server/db/zod-schemas";
+interface CloudflareBindings {
+  DB: D1Database;
+}
 
-const app = new Hono();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.use(accessAuth);
-const route = app.get("/api/health", (c) => {
-  return c.json("Server is healthy!❤️ ");
-});
+const route = app
+  .get("/api/health", (c) => {
+    return c.json("Server is healthy!❤️ ");
+  })
 
+  .post(
+    "/api/subscribe",
+    zValidator("json", insertSubscriberSchema),
+    async (c) => {
+      const { email } = c.req.valid("json");
+
+      if (await isSubscriberExists(c.env.DB, email)) {
+        return c.json({ error: "Email already subscribed" }, 400);
+      }
+
+      const user = await insertSubscriber(c.env.DB, { email });
+      return c.json({
+        message: "Thank you for subscribing!",
+        error: false,
+        user,
+      });
+    },
+  );
 export type AppType = typeof route;
 export default app;
